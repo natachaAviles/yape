094bfde4fec87d65def3388e13cf05ba38d36f88
chinchillapsico <coraline.colous@gmail.com>
2017-08-23 09:41:47 -0300

    agregado el contador

---
+++ public/js/pantalla-3.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+
+    (function(){
+        var counter = 21;
+
+        setInterval(function() {
+            counter--;
+            if (counter >= 0) {
+                span = document.getElementById("count");
+                span.innerHTML = counter;
+            }
+    
+            if (counter === 0) {
+                alert('ahora debería suceder algo');
+                clearInterval(counter);
+            }
+
+        }, 1000);
+
+    })();
+
+}
